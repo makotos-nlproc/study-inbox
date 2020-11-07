52727c5c2b1db97bbc69f6512e9fe5c3089eea00
makotos-nlproc <70269105+makotos-nlproc@users.noreply.github.com>
2020-11-07 17:00:51 +0900

    p51

--- crawl-and-scrap/request_test.py
+++ crawl-and-scrap/request_test.py
@@ -1,10 +1,21 @@
 import requests
 import sys
+import pprint
 
+if len(sys.argv) > 1 and 'http' in sys.argv[1]:
+    response_obj  = requests.get(sys.argv[1])
+    content_type = response_obj.headers.get('content-type')
 
-if len(sys.argv) > 1:
-    # この長さが引数の個数か、単に文字列の長さか確認する
-    response_obj  = requests.get(sys.argv)
-    
+    if 'text/html' in content_type:
+        log_list = [type(response_obj),
+                    content_type,
+                    response_obj.status_code,
+                    response_obj.encoding,
+                    response_obj.text[:6]]
+        pprint.pprint(log_list)
+    elif 'json' in content_type:
+        print(response_obj.json())
+    else:
+        print('no expecting content type')
 else:
     print('no url')
